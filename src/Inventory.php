b250ed69527846ff3b96cb4c360e28016b7a4fd5
rochi88 <29769944+rochi88@users.noreply.github.com>
2026-06-08 08:56:06 +0000

    Fix styling

--- src/Inventory.php
+++ src/Inventory.php
@@ -3678,7 +3678,7 @@ class Inventory
             $result = $query
                 ->leftJoin("{$invTable} as _acct_inv", '_acct_inv.id', '=', "{$soTable}.accounting_invoice_id")
                 ->selectRaw(
-                    "COALESCE(SUM(GREATEST({$soTable}.total_amount - COALESCE(_acct_inv.paid_amount * _acct_inv.exchange_rate, 0), 0)), 0) as unpaid_total"
+                    "COALESCE(SUM(GREATEST({$soTable}.total_amount - COALESCE(_acct_inv.paid_amount * _acct_inv.exchange_rate, 0), 0)), 0) as unpaid_total",
                 )
                 ->first();
 
