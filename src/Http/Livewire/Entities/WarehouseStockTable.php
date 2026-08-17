878034f6cb0138fd81b6662367c091c01519ed2c
rochi88 <raisul.me@gmail.com>
2026-08-17 12:28:26 +0600

    feat: Update warehouse stock table columns for improved data visibility and permissions

--- src/Http/Livewire/Entities/WarehouseStockTable.php
+++ src/Http/Livewire/Entities/WarehouseStockTable.php
@@ -39,23 +39,23 @@ class WarehouseStockTable extends DataTable
             Column::make('Warehouse', 'warehouse.name')->relation('warehouse')->sortable(),
             Column::make('Product', 'product.name')->relation('product')->searchable()
                 ->view('inventory::livewire.partials.warehouse-stock-table.product'),
-            Column::make('On Store', 'qty_on_hand')->format('decimal:2')->sortable()->summable(),
-            Column::make('Saleable', 'net_saleable_stock')->format('decimal:2')->sortable(),
-            Column::make('Reserved', 'qty_reserved')->format('decimal:2')->sortable()->summable(),
-            Column::make('In Transit', 'qty_in_transit')->format('decimal:2')->sortable()->summable()
-                ->can('inventory.stock.manage'),
-            Column::make('WAC', 'wac_amount')->format('decimal:4')
+            Column::make('On Store', 'qty_on_hand')->format('decimal:0')
+                ->can('inventory.stock-data.view-on-hand')->sortable()->summable(),
+            Column::make('Available', 'net_saleable_stock')
+                ->can('inventory.stock-data.view-available')
+                ->format('decimal:0')->sortable(),
+            Column::make('Reserved', 'qty_reserved')->format('decimal:0')
+                ->can('inventory.stock-data.view-reserved')->sortable()->summable(),
+            Column::make('In Transit', 'qty_in_transit')->format('decimal:0')->sortable()->summable()
+                ->can('inventory.stock-data.view-in-transit'),
+            Column::make('WAC', 'wac_amount')->format('decimal:2')
                 ->sortable()->can('inventory.pricing.view-wac'),
             Column::make('B2B Retail', 'b2b_retail_price')->align('right')->excludeFromExport()
                 ->view('inventory::livewire.partials.product-price-table.price'),
-            Column::make('Reorder Point', 'reorder_point')->format('decimal:2')
-                ->sortable()
-                ->hideOnMobile()
-                ->can('inventory.stock.manage'),
             Column::make('Actions')
                 ->view('inventory::livewire.partials.warehouse-stock-table.actions')
                 ->excludeFromExport()
-                ->can('inventory.stock.manage'),
+                ->can('inventory.stock-data.manage'),
         ];
     }
 
